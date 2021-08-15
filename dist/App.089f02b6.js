839975e53de06f97c62a1da0947ca6c365be6fcb
Ivan <sharafutdinov.ivan@gmail.com>
2021-08-15 18:00:13 +0300

    deploy sprint_3

--- dist/App.089f02b6.js
+++ dist/App.089f02b6.js
@@ -3301,7 +3301,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51143" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59363" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
